3443cf84d831cc86b534e918df16a8f6ae5ada3b
Devether15 <josemoya.inf@gmail.com>
2018-12-01 17:27:35 -0300

    añadiendo lugares cercanos y autocompletado, no estàn funcionales

--- js/lugares.js
+++ js/lugares.js
@@ -4,6 +4,18 @@ lugaresModulo = (function () {
     // Completa las direcciones ingresadas por el usuario a y establece los límites
     // con un círculo cuyo radio es de 20000 metros.
   function autocompletar () {
+
+    var miDireccion = document.getElementById('direccion');
+    var limite = new google.maps.Circle({
+      radius: 20000,
+      center: mapa.center,
+      bounds: posicionCentral,
+      map: mapa,
+      strokeColor: 'transparent',
+      fillColor: 'transparent',
+    })
+    completando = new google.maps.places.Autocomplete(miDireccion);
+    completando.setBounds(limite.getBounds());
         /* Completar la función autocompletar(): autocompleta los 4 campos de texto de la
         página (las direcciones ingresables por el usuario).
         Para esto creá un círculo con radio de 20000 metros y usalo para fijar
@@ -16,14 +28,27 @@ lugaresModulo = (function () {
     autocompletar()
   }
 
-    // Busca lugares con el tipo especificado en el campo de TipoDeLugar
+   
 
   function buscarCerca (posicion) {
+
+    var tipoDeLugar = document.getElementById('tipoDeLugar').value;    
+    var radio = document.getElementById('radio').value;  
+    //console.log(radio); 
+    servicioLugares.nearbySearch({
+      location: posicion,
+      radius: radio,
+      type: tipoDeLugar,
+    }, marcadorModulo.marcarLugares());
+  
+
+    
+  }
         /* Completar la función buscarCerca  que realice la búsqueda de los lugares
     del tipo (tipodeLugar) y con el radio indicados en el HTML cerca del lugar
     pasado como parámetro y llame a la función marcarLugares. */
 
-  }
+  
   return {
     inicializar,
     buscarCerca
