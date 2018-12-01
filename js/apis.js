5a67e0d9ffe76c772c342ed2878f5a35c46a6cb0
Devether15 <josemoya.inf@gmail.com>
2018-12-01 11:17:40 -0300

    trabajando la primera guia

---
+++ js/apis.js
@@ -0,0 +1,23 @@
+var mapa // Mapa que vamos a modificar
+
+var posicionCentral = {lat: -34.6083667, lng: -58.37228319999997};
+/* Crear la variable posicionCentral con las coordenadas donde se va a centrar el mapa */
+
+// Inicializa el mapa con un valor de zoom y una locación en el medio
+function inicializarMapa () {
+    
+  mapa = new google.maps.Map(document.getElementById("map"),{
+    zoom: 14, 
+    center: posicionCentral});
+
+/* Modificá la variable mapa con el constructor Map().
+    Tendrás que asignarle un valor de zoom y
+    un centro igual a la variable posicionCentral. */
+  geocodificadorModulo.inicializar()
+  marcadorModulo.inicializar()
+  direccionesModulo.inicializar()
+  lugaresModulo.inicializar()
+  streetViewModulo.inicializar()
+}
+
+
---
+++ js/geocodificador.js
@@ -0,0 +1,33 @@
+geocodificadorModulo = (function () {
+  var geocodificador // Geocodificador que dada una dirección devuelve una coordenada
+  
+    // Permite obtener las coordenadas y las usa con la función llamada por parámtero
+  function usaDireccion (direccion, funcionALlamar) {
+        /* Completar la función usaDireccion(dirección,funcionALlamar)
+     para que se obtengan las coordenadas a partir de la dirección pasada por parámetro
+     y que llame a la función pasada por parámetro con los siguientes parámetros
+     dirección: la dirección pasada por parámetro
+     coordenada: la ubicación de tipo google.maps.LatLng */
+  }
+
+   
+  function inicializar () {
+    var that = this
+    geocodificador = new google.maps.Geocoder()
+
+        // cuando se presiona la tecla enter en el campo direccion, se agrega la dirección y se muestra en el mapa
+    document.querySelector('#direccion').addEventListener('keypress', function (e) {
+      var key = e.which || e.keyCode
+      if (key === 13) { // 13 is enter
+                // code for enter
+        var direccion = document.getElementById('direccion').value
+        that.usaDireccion(direccion, direccionesModulo.agregarDireccionYMostrarEnMapa)
+      }
+    })
+  }
+
+  return {
+    usaDireccion,
+    inicializar
+  }
+})()
