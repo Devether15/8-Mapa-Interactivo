da3d20051c5430315d170788faec0eebb7f75022
Devether15 <josemoya.inf@gmail.com>
2018-12-01 15:17:08 -0300

    añadiendo markador, dragable y animaciòn del marker

--- js/marcador.js
+++ js/marcador.js
@@ -7,6 +7,15 @@ marcadorModulo = (function () {
 
     // Crea un marcador y lo muestra en el mapa
   function mostrarMiMarcador (ubicacion) {
+
+    var miMarcador = new google.maps.Marker({
+      position: ubicacion,
+      map: mapa,
+      title: 'Ubicación',
+      draggable: true,
+      animation: google.maps.Animation.DROP
+    });
+  
         /* Completar la función mostrarMiMarcador() para crear un marcador
         en la posición pasada por parámetro y mostrarlo en el mapa.
         Este marcador debe tener un título, una animación.
